b05935e3f8383be34904332c261343bd26f96c21
chemdemo <yangdemo@gmail.com>
2016-11-03 19:06:50 +0800

    up

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 * @Author: dmyang
 * @Date:   2016-10-11 17:56:02
 * @Last Modified by:   dmyang
-* @Last Modified time: 2016-11-02 16:44:50
+* @Last Modified time: 2016-11-03 11:36:38
 */
 
 'use strict';
@@ -10,42 +10,21 @@
 import http from 'http'
 import path from 'path'
 
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
 import koa from 'koa'
 import kRouter from 'koa-router'
-import { createMemoryHistory, RouterContext, match } from 'react-router'
-import { Provider } from 'react-redux'
-import { trigger } from 'redial'
-import { StyleSheetServer } from 'aphrodite'
 import serve from 'koa-static'
 import webpack from 'webpack'
 import koaWebpackDevMiddleware from 'koa-webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
-// import favicon from './favicon.ico'
 
 import webpackConfig from '../tools/webpack.client.dev'
 import { compileDev } from '../tools/dx'
-import { configureStore } from '../share/store'
-import reducer from '../share/createReducer'
-import createRoutes from '../share/routes'
-
-const promiseMatch = (location) => {
-    return new Promise((resolve, reject) => {
-        match(location, (err, redirectLocation, renderProps) => {
-            if(err) return reject({ err })
-            if(redirectLocation) return reject({ redirectLocation })
-            if(renderProps) return resolve(renderProps)
-            reject()
-        })
-    })
-}
+import setupRoutes from './routes'
 
 const App = (config) => {
     // console.log(config)
-    const __PROD__ = /production|prod/.test(config.nodeEnv)
     const app = koa()
-    const router = kRouter()
+    const router = setupRoutes(kRouter(), app)
 
     app.on('error', (err, ctx) => {
         err.url = err.url || ctx.request.url
@@ -85,82 +64,8 @@ const App = (config) => {
         })
     }
 
-    router.get('*', function*(next) {
-        yield ((callback) => {
-            const store = configureStore({
-                sourceRequest: {
-                    protocol: this.headers['x-forwarded-proto'] || this.protocol,
-                    host: this.headers.host
-                }
-            })
-            const routes = createRoutes(store)
-            const history = createMemoryHistory(this.url)
-            const { dispatch } = store
-
-            match({routes, history}, (err, redirectLocation, renderProps) => {
-                // console.log(err, redirectLocation, renderProps)
-                if (redirectLocation) {
-                    this.redirect(redirectLocation.pathname + redirectLocation.search, '/')
-                    return
-                }
-
-                if (err || !renderProps) {
-                    callback(err)
-                    return
-                }
-
-                const { components } = renderProps
-
-                const locals = {
-                    path: renderProps.location.pathname,
-                    query: renderProps.location.query,
-                    params: renderProps.params,
-                    dispatch
-                }
-
-                trigger('fetch', components, locals)
-                    .then(() => {
-                        const initialState = store.getState()
-                        const InitialView = (
-                            <Provider store={store}>
-                                <RouterContext {...renderProps} />
-                            </Provider>
-                        )
-                        const { html, css} = StyleSheetServer.renderStatic(() => ReactDOMServer.renderToString(InitialView))
-                        const body = `
-                            <!doctype html>
-                            <html>
-                                <head>
-                                    <meta charset="utf-8" />
-                                    <title>react-isomorphic-app</title>
-                                    <style data-aphrodite>${css.content}</style>
-                                    <script>
-                                        window.renderedClassNames = ${JSON.stringify(css.renderedClassNames)};
-                                        window.INITIAL_STATE = ${JSON.stringify(initialState)};
-                                    </script>
-                                </head>
-                                <body>
-                                    <div class="root">${html}</div>
-                                    <script src="${ __PROD__ ? assets.vendor.js : '/vendor.js' }"></script>
-                                    <script async src="${ __PROD__ ? assets.main.js : '/main.js' }" ></script>
-                                </body>
-                            </html>
-                        `
-
-                        this.type = 'text\/html'
-                        this.body = body
-
-                        callback(null)
-                    })
-                    .catch(e => {
-                        callback(e)
-                    })
-            })
-        })
-    })
-
     // use routes
-    app.use(router.routes()).use(router.allowedMethods())
+    app.use(router.routes())//.use(router.allowedMethods())
 
     return app
 }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 * @Author: dmyang
 * @Date:   2016-10-11 17:56:02
 * @Last Modified by:   dmyang
-* @Last Modified time: 2016-11-01 12:12:30
+* @Last Modified time: 2016-11-03 11:43:07
 */
 
 'use strict'
@@ -19,14 +19,15 @@ import App from './app'
 import serverConf from './config'
 
 const staticDir = path.resolve(process.cwd(), 'public')
-
 const config = {...serverConf, staticDir}
+
+global.__PROD__ = /production|prod/.test(config.nodeEnv)
+global.__DEV__ = /development|dev/.test(config.nodeEnv)
+
 const app = App(config)
 const server = http.createServer(app.callback())
 
 server.listen(config.port, (err) => {
-    const __PROD__ = /production|prod/.test(config.nodeEnv)
-
     if(__PROD__) {
         if(err) console.error(err)
     } else {
